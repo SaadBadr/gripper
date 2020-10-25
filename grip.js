bd2ddcbfcabb7119feb7722ee40359279f9d406f
SaadBadr <saadbadr23@gmail.com>
2020-10-26 01:11:13 +0200

    refactoring code, using promises instead of callbacks

--- grip.js
+++ grip.js
@@ -1,21 +1,11 @@
 const request = require("request");
 const cherio = require("cherio");
 const clipboardy = require("clipboardy");
+const fs = require("fs");
+const { promisify } = require("util");
 const url = process.argv[2];
 
-const writeFile = (str) => {
-  const fs = require("fs");
-  const file = fs.writeFile("links.txt", str, (err) => {
-    if (!err) console.log("links are saved to links.txt!");
-    else console.log("ERROR in copying to clipboard\n", err);
-  });
-};
-request(url, (error, response, html) => {
-  if (error) {
-    console.log("INTERNET ERROR!");
-    console.log(error);
-    return;
-  }
+const getUrls = (html) => {
   let list = html.split("</a>");
   list = list.map((el) => {
     return el
@@ -28,10 +18,22 @@ request(url, (error, response, html) => {
   );
 
   list = list.map((el) => url + el);
-  const str = list.join("\n");
+  return list;
+};
+
+request(url, (error, response, html) => {
+  if (error) {
+    console.log("INTERNET ERROR!\n", error);
+    return;
+  }
+  const str = getUrls(html).join("\n");
+
   clipboardy
     .write(str)
-    .then(() => console.log("links are copied to the clipboard!"))
-    .catch((err) => console.log("ERROR in copying to clipboard\n", err));
-  writeFile(str);
+    .then(() => console.log("urls are copied to the clipboard!"))
+    .catch((err) => console.log("ERROR in copying to clipboard!\n", err));
+
+  promisify(fs.writeFile)("links.txt", str)
+    .then(() => console.log("urls are saved to links.txt!"))
+    .catch((err) => console.log("ERROR in saving links to clipboard!\n", err));
 });
