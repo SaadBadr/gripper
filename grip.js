8ca32c1baa919e10c48708305632ff73d7286d49
SaadBadr <saadbadr23@gmail.com>
2020-10-26 02:30:24 +0200

    using cheerio instead of html string manipulation

--- grip.js
+++ grip.js
@@ -1,44 +1,44 @@
-const request = require("request");
-const cherio = require("cherio");
-const clipboardy = require("clipboardy");
-const fs = require("fs");
-const { promisify } = require("util");
+const request = require('request');
+const cheerio = require('cheerio');
+const clipboardy = require('clipboardy');
+const fs = require('fs');
+const { promisify } = require('util');
 const url = process.argv[2];
 
+const verifyUrl = (el) =>
+  el && !el.startsWith('../') && !el.startsWith('?') && !el.startsWith('/');
+
 const getUrls = (html) => {
-  let list = html.split("</a>");
-  list = list.map((el) => {
-    return el
-      .replace(el, el.match(/<a\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1/))
-      .split('"')[1];
+  const $ = cheerio.load(html);
+
+  let list = [];
+
+  $('a').each((index, value) => {
+    const el = $(value).attr('href');
+    if (verifyUrl(el)) list.push(url + el);
   });
 
-  list = list.filter(
-    (el) => el != "../" && el && !el.startsWith("?") && !el.startsWith("/")
-  );
-
-  list = list.map((el) => url + el);
   return list;
 };
 
 request(url, (error, response, html) => {
   if (error) {
-    console.log("INTERNET ERROR!\n", error);
+    console.log('INTERNET ERROR!\n', error);
     return;
   }
 
-  const str = getUrls(html).join("\n");
+  const str = getUrls(html).join('\n');
 
-  if (!process.argv.includes("-nocopy"))
+  if (!process.argv.includes('-nocopy'))
     clipboardy
       .write(str)
-      .then(() => console.log("urls are copied to the clipboard!"))
-      .catch((err) => console.log("ERROR in copying to clipboard!\n", err));
+      .then(() => console.log('urls are copied to the clipboard!'))
+      .catch((err) => console.log('ERROR in copying to clipboard!\n', err));
 
-  if (!process.argv.includes("-nosave"))
-    promisify(fs.writeFile)("links.txt", str)
-      .then(() => console.log("urls are saved to links.txt!"))
+  if (!process.argv.includes('-nosave'))
+    promisify(fs.writeFile)('links.txt', str)
+      .then(() => console.log('urls are saved to links.txt!'))
       .catch((err) =>
-        console.log("ERROR in saving links to clipboard!\n", err)
+        console.log('ERROR in saving links to clipboard!\n', err)
       );
 });
